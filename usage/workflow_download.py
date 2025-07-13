8816609ad6b8b937cd635290ba5aa7f3cc39d6a7
lazyeight123 <56684553+lazyeight123@users.noreply.github.com>
2025-07-14 00:23:19 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -3,11 +3,11 @@ from jmcomic.cl import JmcomicUI
 
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
 jm_albums = '''
-JM1196425
-JM1192978
-JM1057738
-JM504886
-JM1194917
+JM1194233
+JM1192984
+JM1150169
+JM1194285
+JJM301258
 
 
 
